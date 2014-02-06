654086b3079f996b6eff02f5ce9c07a211ba391b
pe7er <info@db8.nl>
2014-02-06 14:31:01 +0100

    first version of db8socialmediashare

---
+++ mod_db8socialmediashare.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @package	mod_db8socialmediashare
+ * @author	Peter Martin, www.db8.nl
+ * @copyright	Copyright (C) 2014 Peter Martin. All rights reserved.
+ * @license	GNU General Public License version 2 or later.
+ */
+defined('_JEXEC') or die;
+
+$url = JURI::current();
+$document = JFactory::getDocument();
+$title = str_replace('&', 'en', $document->getTitle("title"));
+$document->addStyleSheet('modules/mod_db8socialmediashare/tmpl/db8socialmediashare.css');
+
+$socialmedia = array(
+    array(
+        "medium" => "twitter",
+        "order" => $params->get('twitter'),
+        "mouseover" => JText::_('MOD_DB8SOCIALMEDIASHARE_TWITTER'), $params->get('twitterlinktext'),
+        "htmlclass" => "twitterlink",
+        "href" => "http://twitter.com/home?status=" . $title . " " . $url
+    ),
+    array(
+        "medium" => "facebook",
+        "order" => $params->get('facebook'),
+        "mouseover" => JText::_('MOD_DB8SOCIALMEDIASHARE_FACEBOOK'),
+        "htmlclass" => "facebooklink",
+        "href" => "http://facebook.com/sharer.php?u=" . $url . "&amp;t=" . $title
+    ),
+    array(
+        "medium" => "linkedin",
+        "order" => $params->get('linkedin'),
+        "mouseover" => JText::_('MOD_DB8SOCIALMEDIASHARE_LINKEDIN'),
+        "htmlclass" => "linkedinlink",
+        "href" => "http://www.linkedin.com/shareArticle?mini=true&url=" . $url . "&title=" . $title . "&ro=false"
+    ),
+    array(
+        "medium" => "googleplus",
+        "order" => $params->get('googleplus'),
+        "mouseover" => JText::_('MOD_DB8SOCIALMEDIASHARE_GOOGLEPLUS'),
+        "htmlclass" => "googlepluslink",
+        "href" => "https://plus.google.com/share?url=" . $url
+    )
+);
+
+require JModuleHelper::getLayoutPath('mod_db8socialmediashare', 'default');
---
+++ tmpl/default.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package	mod_db8socialmediashare
+ * @author	Peter Martin, www.db8.nl
+ * @copyright	Copyright (C) 2014 Peter Martin. All rights reserved.
+ * @license	GNU General Public License version 2 or later.
+ */
+defined('_JEXEC') or die;
+?>
+<div class="db8socialmediashare<?php echo $params->get('moduleclass_sfx'); ?>">
+    <?php
+    for ($i = 1; $i <= 4; $i++) {
+        for ($a = 0; $a <= 3; $a++) {
+            if ($socialmedia[$a]['order'] == $i) {
+                ?>
+                <a title="<?php echo $socialmedia[$a]['mouseover']; ?>" class="<?php echo $socialmedia[$a]['htmlclass']; ?>" href="<?php echo $socialmedia[$a]['href']; ?>" target="_blank">&nbsp;</a>
+                <?php
+            }
+        }
+    }
+    ?>
+</div>
